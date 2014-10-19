1675c5452bc3307e5506983f4d3177c0d3167396
Drew Stevens <wastevens@gmail.com>
2014-10-19 16:29:03 -0400

    Successful build and deployment of hello world responder!

---
+++ GiftListWebServices/src/java/com/dstevens/rest/controllers/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.dstevens.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String showIndex() {
+    	return "Hello, world!";
+    }
+    
+    
+}
